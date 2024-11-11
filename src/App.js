defdae16455293581b688c0e9337a0c90654fda6
PALLE Durga Prasad Reddy <pdurga160@gmail.com>
2024-11-11 20:15:09 +0530

    Logic for calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Wrapper from './Wrapper';
+import Screen from './Screen';
+import ButtonBox from './ButtonBox';
+import Button from './Button';
 
-function App() {
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const App = () => {
+  const [calc, setCalc] = useState({ sign: "", num: 0, res: 0 });
+
+  const handleClick = (value) => {
+    // Handle button click logic here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Wrapper>
+      <Screen value={calc.num ? calc.num : calc.res} />
+      <ButtonBox>
+        {btnValues.flat().map((btn, i) => (
+          <Button
+            key={i}
+            className={btn === "=" ? "equals" : ""}
+            value={btn}
+            onClick={() => handleClick(btn)}
+          />
+        ))}
+      </ButtonBox>
+    </Wrapper>
   );
-}
+};
 
 export default App;
